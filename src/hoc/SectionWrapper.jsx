047daa75b3f4807702c2de985f7a9baeed099fae
MauLight <maulisseluz@gmail.com>
2023-04-27 15:15:23 -0400

    timeline added

--- src/hoc/SectionWrapper.jsx
+++ src/hoc/SectionWrapper.jsx
@@ -3,7 +3,7 @@ import { motion } from "framer-motion";
 import { styles } from "../style";
 import { staggerContainer } from "../utils/motion";
 
-const StarWrapper = (Component, idName) =>
+const SectionWrapper = (Component, idName) =>
   function HOC() {
     return (
       <motion.section
@@ -22,5 +22,5 @@ const StarWrapper = (Component, idName) =>
     );
   };
 
-export default StarWrapper;
+export default SectionWrapper;
 
