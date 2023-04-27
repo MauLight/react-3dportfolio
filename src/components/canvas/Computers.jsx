8a94271174c35ffe22251d1570d8f7acf4a3c008
MauLight <maulisseluz@gmail.com>
2023-04-26 22:35:55 -0400

    resize of 3d model added

--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -6,9 +6,13 @@ import CanvasLoader from '../Loader'
 
 
 
-const Computers = () => {
+const Computers = ({ isMobile }) => {
 
   const computer = useGLTF('./desktop_pc/scene.gltf')
+  const oldcomputer = useGLTF('./old_computers/scene.gltf')
+  const blackhole = useGLTF('./black_hole/scene.gltf')
+  const police = useGLTF('./police/scene.gltf')
+  const walkman = useGLTF('./walkman/scene.gltf')
 
 
   return (
@@ -18,22 +22,54 @@ const Computers = () => {
         groundColor="black"
       />
       <pointLight intensity={1} />
+      <spotLight
+        position={[-20, 50, 10]}
+        angle={0.12}
+        penumbra={1}
+        intensity={1}
+        castShadow
+        shadow-mapSize={1024}
+      />
       <primitive
-        object={computer.scene}
-        scale={0.75}
-        position={[0, -3.25, -1.5]}
-        rotation={[-0.007, -0.2, -0.05]}
+        object={walkman.scene}
+        scale={isMobile ? 0.65 : 0.85}
+        position={isMobile ? [-13, -5.25, -5] : [-5, -5.25, -5]}
+        rotation={[0, -5.5, 0]}
       />
     </mesh>
   )
 }
 
 const ComputerCanvas = () => {
+
+  const [isMobile, setIsMobile] = useState(false)
+
+  useEffect(() => {
+
+    //Add a listener for changes to the screen size
+    const mediaQuery = window.matchMedia("(max-width: 700px)")
+
+    //Set the initial value of the isMobile state variable
+    setIsMobile(mediaQuery.matches)
+
+    //Define a callback function to handle changes to the mediaQuery
+    const handleMediaQueryChange = (e) => {
+      setIsMobile(e.matches)
+    }
+    mediaQuery.addEventListener('change', handleMediaQueryChange)
+
+    //Remove the listener when the component is unmounted
+    return () => {
+      mediaQuery.removeEventListener('change', handleMediaQueryChange)
+    }
+  }, [])
+
+
   return (
     <Canvas
       frameloop='demand'
       shadows
-      camera={{ position: [20, 3, 5], fov: 25 }}
+      camera={{ position: [30, 10, 15], fov: 25 }}
       gl={{ preserveDrawingBuffer: true }}
     >
       <Suspense fallback={<CanvasLoader />}>
@@ -42,7 +78,7 @@ const ComputerCanvas = () => {
           maxPolarAngle={Math.PI / 2}
           minPolarAngle={Math.PI / 2}
         />
-        <Computers />
+        <Computers isMobile={isMobile} />
       </Suspense>
       <Preload all />
     </Canvas>
